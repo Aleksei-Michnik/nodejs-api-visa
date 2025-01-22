2c1ef36b63a4b74631048c9635d174398d366a22
Aleksei Michnik <alexmich777@gmail.com>
2025-01-22 09:05:03 +0200

    Add missing http-client provider

--- visa-payment-api/src/controllers/payment.module.ts
+++ visa-payment-api/src/controllers/payment.module.ts
@@ -3,12 +3,13 @@ import { PaymentController } from './payment.controller';
 import { PaymentService } from '../services/payment.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PaymentSchema } from '../schemas/payment.schema';
+import { HttpClient } from '../utils/http-client';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: 'Payment', schema: PaymentSchema }]),
     ],
     controllers: [PaymentController],
-    providers: [PaymentService],
+    providers: [PaymentService, HttpClient],
 })
 export class PaymentModule {}
