149c4171a804c40c88f2cb7e3cc5f68cdd9f836d
Aleksei Michnik <alexmich777@gmail.com>
2025-01-22 08:44:21 +0200

    Add client utility to make HTTP requests to the Mock Visa API

---
+++ visa-payment-api/src/utils/http-client.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+
+@Injectable()
+export class HttpClient {
+    private readonly client: AxiosInstance;
+
+    constructor() {
+        this.client = axios.create({
+            baseURL: process.env.MOCK_API_URL || 'http://localhost:3001',
+        });
+    }
+
+    async post(path: string, data: any): Promise<any> {
+        const response = await this.client.post(path, data);
+        return response.data;
+    }
+}
