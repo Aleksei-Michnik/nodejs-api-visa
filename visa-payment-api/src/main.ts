1163ca648c6dccff56a46881c50fb168cc2a96dc
Aleksei Michnik <alexmich777@gmail.com>
2025-02-01 19:14:29 +0200

    Enable CORS and add request logging middleware

--- visa-payment-api/src/main.ts
+++ visa-payment-api/src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.VISA_PAYMENT_API_PORT ?? 3002, '0.0.0.0');
+
+  app.enableCors({
+    origin: [`http://localhost:${process.env.VITE_PORT_HOST}`],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  app.use((req: { method: any; url: any; }, res: any, next: () => void) => {
+    console.log(`[${req.method}] ${req.url}`);
+    next();
+  });
+
+  await app.listen(process.env.VISA_PAYMENT_API_PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
