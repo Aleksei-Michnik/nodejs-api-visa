b6b67c95b0653a5bea0ade98efca9c765b1ffbd0
Aleksei Michnik <alexmich777@gmail.com>
2025-02-07 15:56:59 +0200

    Reorder imports in PaymentService for consistency

--- visa-payment-api/src/services/payment.service.ts
+++ visa-payment-api/src/services/payment.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { HttpClient } from '../utils/http-client';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { HttpClient } from '../utils/http-client';
 
 @Injectable()
 export class PaymentService {
